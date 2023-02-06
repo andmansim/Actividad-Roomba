c3c4c45fd4493f5d3a135fe7bdd1cdb51e8ded8c
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-06 16:15:50 +0100

    girar

--- ventanas.py
+++ ventanas.py
@@ -3,5 +3,10 @@ class Coche:
     def rodar(self):
         print('Avanza el coche')
         for i in range(10):#Hace que el coche avance 10 veces con un segundo de separación
+            print('.')
             time.sleep(1)
             
+    def girar(self):
+        print('Giramos a la derecha') #Esto lo hacemos durante un segundo
+        time.sleep(1)#Esperamos un seg
+        
