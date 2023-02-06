dd1c76a54c334e4d59ffd5c3bbf3819b3735a6d3
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-06 17:03:24 +0100

    añadimos artefactos (genshin)

--- ventanas.py
+++ ventanas.py
@@ -1,13 +1,33 @@
-from tkinter import Frame
+from tkinter import Frame, Label, Scale
+from tkinter import LEFT, HORIZONTAL
+
 
 '''ventana = Frame()
 ventana.mainloop() '''#Con esto crea una ventana y la deja abierta con el bucle
 
-class Ventana(Frame):
+'''class Ventana(Frame):
     def __init__(self, master = None):
         super(Ventana, self).__init__(master, width = 320, height = 240) #Indicamos el tamaño
         self.master.title('Graficazos') #Le ponemos nombre a nuestra ventana
         self.pack()
 
 ventana = Ventana() #Ventana digievolucionada
-ventana.mainloop()
+ventana.mainloop()'''
+
+class Ventana(Frame):
+    def __init__(self, master = None):
+        super(Ventana, self).__init__(master, width = 320, height = 240) #Indicamos el tamaño
+        self.master.title('Conversor C <-> F') #Le ponemos nombre a nuestra ventana
+        self.pack()
+        
+    def initWidgets(self): #Creamos dos barritas, F y C que se mueven en escalas distintas
+        self.FTexto = Label(self, text = 'F')
+        self.FCursor = Scale(self, orient = HORIZONTAL, from_ = -148, to = 212)
+        self.CTexto = Label(self, text = 'C')
+        self.CCursor = Scale(self, orient = HORIZONTAL, from_ = -100, to = 100)
+        for widget in [self.CTexto, self.CCursor, self.FTexto, self.FCursor]:
+            widget.pack(side=LEFT)
+            
+ventanuca = Ventana()
+ventanuca.initWidgets()
+ventanuca.mainloop() 
