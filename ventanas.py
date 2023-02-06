5418e572b58312a8dbe7beb0fdcf860d5d94d133
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-06 16:51:43 +0100

    digimon

--- ventanas.py
+++ ventanas.py
@@ -1,4 +1,13 @@
 from tkinter import Frame
 
-ventana = Frame()
+'''ventana = Frame()
+ventana.mainloop() '''#Con esto crea una ventana y la deja abierta con el bucle
+
+class Ventana(Frame):
+    def __init__(self, master = None):
+        super(Ventana, self).__init__(master, width = 320, height = 240) #Indicamos el tamaño
+        self.master.title('Graficazos') #Le ponemos nombre a nuestra ventana
+        self.pack()
+
+ventana = Ventana() #Ventana digievolucionada
 ventana.mainloop()
