74985c5b91457fe41b2b417481b4419d51c2e379
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-06 17:11:37 +0100

    #aydua

--- ventanas.py
+++ ventanas.py
@@ -22,12 +22,24 @@ class Ventana(Frame):
         
     def initWidgets(self): #Creamos dos barritas, F y C que se mueven en escalas distintas
         self.FTexto = Label(self, text = 'F')
-        self.FCursor = Scale(self, orient = HORIZONTAL, from_ = -148, to = 212)
+        self.FCursor = Scale(self, orient = HORIZONTAL, from_ = -148, to = 212, command= self.convertirFEnC) #añadimos el command después
         self.CTexto = Label(self, text = 'C')
-        self.CCursor = Scale(self, orient = HORIZONTAL, from_ = -100, to = 100)
+        self.CCursor = Scale(self, orient = HORIZONTAL, from_ = -100, to = 100, command= self.convertirCEnF)
         for widget in [self.CTexto, self.CCursor, self.FTexto, self.FCursor]:
             widget.pack(side=LEFT)
-            
+           
+    #Estas dos funciones sirven para que al mover uno de los dos cursores, el otro se mueva simultáneamente        
+    def convertirCEnF(self, valor):
+        C = float(valor)
+        F = C * 9/5 + 32
+        self.FCursor.set(F)
+        
+    def convertirFEnC(self, valor):
+        F = float(valor)
+        C = (F-32)*5/9 
+        self.CCursor.set(C)
+    
 ventanuca = Ventana()
 ventanuca.initWidgets()
 ventanuca.mainloop() 
+
