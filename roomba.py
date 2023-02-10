89e2c0e4890b7c1ef6e5814c691c3a263752c5c4
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-10 12:07:57 +0100

    zona 1

--- roomba.py
+++ roomba.py
@@ -3,34 +3,27 @@ from tkinter import*
 
 class Ventana(Frame):
     def __init__(self, master = None):
-        #super(Ventana, self).__init__(master, width = 500, height = 630) #Indicamos el tamaño
         r = Tk()
-        r.title('Graficazos') #Le ponemos nombre a nuestra ventana
+        r.title('Graficazos') 
         self.c = Canvas(master = r, width = 500, height = 630)
         self.c.pack()
     def pintar(self):
         t = RawTurtle(self.c)
+        t.pencolor('white')
+        t.goto(-250, 165)
+        
+        #Zona 1
+        t.fillcolor("green")
         t.pencolor('green')
-        t.forward(50)
+        t.begin_fill()
+        t.goto(250, 165)
+        t.goto(250, 315)
+        t.goto(-250, 315)
+        t.goto(-250, 165)
+        t.end_fill()
+        
         self.c.mainloop()
 
 ventana = Ventana() #Ventana digievolucionada
 ventana.pintar()
-
-
-
-'''
-
-r = Tk()
-r.title('P')
-c = Canvas(master = r, width = 500, height = 630)
-c.pack()
-t = RawTurtle(c)
-t.pencolor('green')
-t.forward(50)
-c.mainloop()
-
-
-'''
-
 
