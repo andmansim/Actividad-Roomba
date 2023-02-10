52f01537332d15853c6b17a317f138ed9a73a561
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-10 11:43:54 +0100

    calor

--- roomba.py
+++ roomba.py
@@ -1,14 +1,19 @@
 from turtle import*
 import tkinter as tk
 
-r = tk.Tk()
-r.title('Patata')
-ventana = tk.Canvas(master=r, width=500, height=500)
-ventana.pack()
+class Ventana(Frame):
+    def __init__(self, master = None):
+        super(Ventana, self).__init__(master, width = 500, height = 630) #Indicamos el tamaño
+        self.master.title('Graficazos') #Le ponemos nombre a nuestra ventana
+        self.pack()
 
+ventana = Ventana() #Ventana digievolucionada
 t = RawTurtle(ventana)
 t.pencolor('green')
 t.forward(50)
+ventana.mainloop()
 
-r.mainloop()
+
+
+
 
