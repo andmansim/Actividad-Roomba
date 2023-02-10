98bd4b3a86f38993ccacc40ae86867e1924be373
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-10 11:56:21 +0100

    dbsjdxdbjv

--- roomba.py
+++ roomba.py
@@ -3,17 +3,34 @@ from tkinter import*
 
 class Ventana(Frame):
     def __init__(self, master = None):
-        super(Ventana, self).__init__(master, width = 500, height = 630) #Indicamos el tamaño
-        self.master.title('Graficazos') #Le ponemos nombre a nuestra ventana
-        self.pack()
+        #super(Ventana, self).__init__(master, width = 500, height = 630) #Indicamos el tamaño
+        r = Tk()
+        r.title('Graficazos') #Le ponemos nombre a nuestra ventana
+        self.c = Canvas(master = r, width = 500, height = 630)
+        self.c.pack()
+    def pintar(self):
+        t = RawTurtle(self.c)
+        t.pencolor('green')
+        t.forward(50)
+        self.c.mainloop()
 
 ventana = Ventana() #Ventana digievolucionada
-t = RawTurtle(ventana)
+ventana.pintar()
+
+
+
+'''
+
+r = Tk()
+r.title('P')
+c = Canvas(master = r, width = 500, height = 630)
+c.pack()
+t = RawTurtle(c)
 t.pencolor('green')
 t.forward(50)
-ventana.mainloop()
+c.mainloop()
 
 
-
+'''
 
 
