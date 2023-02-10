cd1599d39493abf0fd34aad90fd4191b0602db74
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-10 12:32:32 +0100

    pasando a poo

--- roomba.py
+++ roomba.py
@@ -7,25 +7,31 @@ class Ventana(Frame):
         r.title('Graficazos') 
         self.c = Canvas(master = r, width = 500, height = 630)
         self.c.pack()
-    def pintar(self):
-        t = RawTurtle(self.c)
-        t.penup()
-        t.goto(-250, 165)
-        t.pendown()
+        self.t = RawTurtle(self.c)
+    def zona(self, color, x, y, x1, y1):
         
-        #Zona 1
-        t.fillcolor("green")
-        t.pencolor('green')
-        t.begin_fill()
-        t.goto(250, 165)
-        t.goto(250, 315)
-        t.goto(-250, 315)
-        t.goto(-250, 165)
-        t.end_fill()
+        self.t.fillcolor(color)
+        self.t.pencolor(color)
+        self.t.begin_fill()
+        self.t.goto(x, y)
+        self.t.goto(x, y1)
+        self.t.goto(x1, y1)
+        self.t.goto(x1, y)
+        self.t.end_fill()
+        
+        '''
+        self.t.fillcolor("pink")
+        self.t.pencolor('pink')
+        self.t.begin_fill()
+        self.t.goto(250, 165)
+        self.t.goto(250, 315)
+        self.t.goto(-250, 315)
+        self.t.goto(-250, 165)
+        self.t.end_fill()
         
         #Zona 2
-        t.fillcolor("orange")
-        t.pencolor('orange')
+        t.fillcolor("light green")
+        t.pencolor('light green')
         t.begin_fill()
         t.goto(-250, -315)
         t.goto(-149, -315)
@@ -34,11 +40,11 @@ class Ventana(Frame):
         t.end_fill()
         
         #Zona 3
-        t.fillcolor("blue")
+        t.fillcolor("light blue")
         t.penup()
         t.goto(-59,165)
         t.pendown()
-        t.pencolor('blue')
+        t.pencolor('light blue')
         t.begin_fill()
         t.goto(-59, -315)
         t.goto(250, -315)
@@ -47,11 +53,11 @@ class Ventana(Frame):
         t.end_fill()
         
         #Zona 4
-        t.fillcolor("purple")
+        t.fillcolor("violet")
         t.penup()
         t.goto(-59,-315)
         t.pendown()
-        t.pencolor('purple')
+        t.pencolor('violet')
         t.begin_fill()
         t.goto(-59, -95)
         t.goto(-149, -95)
@@ -60,7 +66,12 @@ class Ventana(Frame):
         t.end_fill()
         
         self.c.mainloop()
-
+        '''
+        
 ventana = Ventana() #Ventana digievolucionada
-ventana.pintar()
+ventana.zona('pink', 250, 165, -250, 315)
+ventana.zona('pink', 250, 165, -250, 315)
+ventana.zona('pink', 250, 165, -250, 315)
+ventana.zona('pink', 250, 165, -250, 315)
 
+ventana.mainloop()
