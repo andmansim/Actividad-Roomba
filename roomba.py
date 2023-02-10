878b02062080554a9e66380dc15143093b6c9874
andmansim <91722324+andmansim@users.noreply.github.com>
2023-02-10 11:39:47 +0100

    ya

--- roomba.py
+++ roomba.py
@@ -1,34 +1,14 @@
-'''from tkinter import Frame, Label, Scale
-from tkinter import LEFT, HORIZONTAL'''
- 
+from turtle import*
+import tkinter as tk
 
-'''class Ventana(Frame):
-    def __init__(self, master = None):
-        super(Ventana, self).__init__(master, width = 500, height = 630) #Indicamos el tamaño
-        self.master.title('Habitación a limpiar') #Le ponemos nombre a nuestra ventana
-        self.pack()
-        '''
-'''def initWidgets(self): #Creamos dos barritas, F y C que se mueven en escalas distintas
-        self.FTexto = Label(self, text = 'F')
-        self.FCursor = Scale(self, orient = HORIZONTAL, from_ = -148, to = 212, command= self.convertirFEnC) #añadimos el command después
-        self.CTexto = Label(self, text = 'C')
-        self.CCursor = Scale(self, orient = HORIZONTAL, from_ = -100, to = 100, command= self.convertirCEnF)
-        for widget in [self.CTexto, self.CCursor, self.FTexto, self.FCursor]:
-            widget.pack(side=LEFT)'''
-           
-    #Estas dos funciones sirven para que al mover uno de los dos cursores, el otro se mueva simultáneamente        
-'''def convertirCEnF(self, valor):
-        C = float(valor)
-        F = C * 9/5 + 32
-        self.FCursor.set(F)
-        
-    def convertirFEnC(self, valor):
-        F = float(valor)
-        C = (F-32)*5/9 
-        self.CCursor.set(C)
-    
-ventanuca = Ventana()
-#ventanuca.initWidgets()
-ventanuca.mainloop() '''
+r = tk.Tk()
+r.title('Patata')
+ventana = tk.Canvas(master=r, width=500, height=500)
+ventana.pack()
 
+t = RawTurtle(ventana)
+t.pencolor('green')
+t.forward(50)
+
+r.mainloop()
 
