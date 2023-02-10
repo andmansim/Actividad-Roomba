a746ed9e9e1cf737c2c13f10c99771fb60f439cc
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2023-02-10 11:17:46 +0100

    cositas

--- roomba.py
+++ roomba.py
@@ -1,13 +1,16 @@
-from tkinter import Frame, Label, Scale
-from tkinter import LEFT, HORIZONTAL
+'''from tkinter import Frame, Label, Scale
+from tkinter import LEFT, HORIZONTAL'''
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys 
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QSpinBox, QLineEdit, QHBoxLayout, QComboBox 
 
-class Ventana(Frame):
+'''class Ventana(Frame):
     def __init__(self, master = None):
         super(Ventana, self).__init__(master, width = 500, height = 630) #Indicamos el tamaño
         self.master.title('Habitación a limpiar') #Le ponemos nombre a nuestra ventana
         self.pack()
-        
-    '''def initWidgets(self): #Creamos dos barritas, F y C que se mueven en escalas distintas
+        '''
+'''def initWidgets(self): #Creamos dos barritas, F y C que se mueven en escalas distintas
         self.FTexto = Label(self, text = 'F')
         self.FCursor = Scale(self, orient = HORIZONTAL, from_ = -148, to = 212, command= self.convertirFEnC) #añadimos el command después
         self.CTexto = Label(self, text = 'C')
@@ -16,7 +19,7 @@ class Ventana(Frame):
             widget.pack(side=LEFT)'''
            
     #Estas dos funciones sirven para que al mover uno de los dos cursores, el otro se mueva simultáneamente        
-    def convertirCEnF(self, valor):
+'''def convertirCEnF(self, valor):
         C = float(valor)
         F = C * 9/5 + 32
         self.FCursor.set(F)
@@ -28,4 +31,14 @@ class Ventana(Frame):
     
 ventanuca = Ventana()
 #ventanuca.initWidgets()
-ventanuca.mainloop() 
+ventanuca.mainloop() '''
+
+
+class MainWindow(QMainWindow): 
+    def __init__(self): 
+        super(MainWindow, self).__init__() 
+ 
+app = QApplication(sys.argv) 
+window = MainWindow() 
+window.show()
+sys.exit(app.exec_()) 
