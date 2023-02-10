60f6b4e8a42cd95c08194f4e4c01188b02b89555
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2023-02-10 11:07:18 +0100

    ventana grande hecha

--- roomba.py
+++ roomba.py
@@ -7,13 +7,13 @@ class Ventana(Frame):
         self.master.title('Habitación a limpiar') #Le ponemos nombre a nuestra ventana
         self.pack()
         
-    def initWidgets(self): #Creamos dos barritas, F y C que se mueven en escalas distintas
+    '''def initWidgets(self): #Creamos dos barritas, F y C que se mueven en escalas distintas
         self.FTexto = Label(self, text = 'F')
         self.FCursor = Scale(self, orient = HORIZONTAL, from_ = -148, to = 212, command= self.convertirFEnC) #añadimos el command después
         self.CTexto = Label(self, text = 'C')
         self.CCursor = Scale(self, orient = HORIZONTAL, from_ = -100, to = 100, command= self.convertirCEnF)
         for widget in [self.CTexto, self.CCursor, self.FTexto, self.FCursor]:
-            widget.pack(side=LEFT)
+            widget.pack(side=LEFT)'''
            
     #Estas dos funciones sirven para que al mover uno de los dos cursores, el otro se mueva simultáneamente        
     def convertirCEnF(self, valor):
@@ -27,5 +27,5 @@ class Ventana(Frame):
         self.CCursor.set(C)
     
 ventanuca = Ventana()
-ventanuca.initWidgets()
+#ventanuca.initWidgets()
 ventanuca.mainloop() 
